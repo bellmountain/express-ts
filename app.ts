8216a6580f074b1b2c455c02e711fddcb6439b4e
Dennis Kroeger <d.kroeger@bellmountain-media.de>
2016-11-19 13:12:09 +0100

    mongoose db connected

--- app.ts
+++ app.ts
@@ -12,6 +12,8 @@ var app = express();
 var mongoose = require('mongoose');
 
 mongoose.connect("mongodb://localhost/mydb");
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'))
 
 // view engine setup
 
